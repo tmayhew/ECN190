732af83b9f1d7d9a886dd072c5371d41722f2810
John Mayhew <jmmayhew@ucdavis.edu>
2020-04-09 15:01:57 -0700

    New csv files.

--- homework2Trey.R
+++ homework2Trey.R
@@ -1,5 +1,6 @@
-tableb1 = read.csv("tableb1.csv")[,-1]
-tableb27 = read.csv("tableb27.csv")[,-1]
+tableb1 = read.csv("GDPdata.csv")[,-1]
+tableb27 = read.csv("Unemployment.csv")[,-1]
+
 USeconomy = cbind.data.frame(year = tableb1$Yr, gdpgrowth = tableb1$GDP, ue = tableb27$Unemployment.Rate)
 tail(USeconomy)
 
