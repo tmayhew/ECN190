eea4467f79a8d00e300e4ce11fd390710d97f0e9
John Mayhew <jmmayhew@ucdavis.edu>
2020-04-19 13:17:22 -0700

    plotted maxtemp data

--- homework3MAIN.R
+++ homework3MAIN.R
@@ -1,2 +1,7 @@
-# https://zoom.us/j/99316268734?pwd=d0QrS2dmWllYM0tNNiswUzlkbEN1QT09
+library(tidyverse)
+library(ggthemes)
+
 DavisWeather = read.csv("DavisWeather.csv")[,-1]
+DavisWeather$t = 1:nrow(DavisWeather)
+DavisWeather %>% ggplot(aes(x = t, y = maxtemp)) + geom_line() + theme_bw() +
+  scale_x_continuous("Time") + scale_y_continuous("Maximum Temperature")
